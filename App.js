0043a02df88c40cf5d22d6ad08ea67aa5a82010a
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-16 01:53:43 +0100

    mess

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Alert} from 'react-native';
 
 export default function App() {
   const [start_text, onChangeNumber] = React.useState('');
+  var arr = [];
+  const show_res = () => {
+    arr.push(start_text);
+    onChangeNumber('');
+    Alert.alert(arr);
+  }
   return (
     <SafeAreaView>
       <TextInput
         style = {styles.input}
-        onChangeText={onChangeNumber}
-        value={start_text}
+        onChangeText={(value) => onChangeNumber(value)}
         placeholder='Start Entering choices'
       />
+      <Button title = "Press me" style = {st_button.button} onPress={()=>show_res()}>
+      </Button>
+      <Text>Hello {start_text}</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -23,6 +31,14 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    top: '50%'
+    top: 100
+  }
+});
+
+const st_button = StyleSheet.create({
+  button: {
+    height: 50,
+    padding: 10,
+    top: 150
   }
 });
